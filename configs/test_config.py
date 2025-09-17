30295690138f4ee50adc209e611d8d88a6034a9c
Esben-Andreas-Madsen <mainmaxen@gmail.com>
2025-09-17 14:21:38 +0200

    simplified and changed to in-memory db

--- configs/test_config.py
+++ configs/test_config.py
@@ -1,9 +1,4 @@
-import os
-
-PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-DB_PATH = os.path.join(PROJECT_ROOT, "app", "storage", "test_library.db")
-
 class TestConfig:
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{DB_PATH}"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
